dab0a4f19789ac8483fde081c98d937c0e991ef1
juliusbay <juax0001@stud.kea.dk>
2025-04-30 10:33:43 +0200

    Initial commit

---
+++ src/main/java/org/example/eksamensprojekt2semester/Eksamensprojekt2SemesterApplication.java
@@ -0,0 +1,13 @@
+package org.example.eksamensprojekt2semester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksamensprojekt2SemesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eksamensprojekt2SemesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/eksamensprojekt2semester/Eksamensprojekt2SemesterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eksamensprojekt2semester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eksamensprojekt2SemesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
