81eef3d12ed7ae8213aa1cc3f5c4d8af6039211c
juliusbay <juax0001@stud.kea.dk>
2025-04-30 10:34:47 +0200

    first commit

--- src/main/java/org/example/eksamensprojekt2semester/Eksamensprojekt2SemesterApplication.java
+++ src/main/java/org/example/eksamensprojekt2semester/Eksamensprojekt2SemesterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Eksamensprojekt2SemesterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Eksamensprojekt2SemesterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Eksamensprojekt2SemesterApplication.class, args);
+    }
 
 }
