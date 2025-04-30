65a3f4d2e13cec1a589885a0b6ddff16bbbbb684
Mr.Morsing14 <frmo0006@stud.kea.dk>
2025-04-30 13:02:18 +0200

    Lavet Booking Model og Car Model

---
+++ src/main/java/org/example/eksamensprojekt2semester/Model/BookingModel.java
@@ -0,0 +1,114 @@
+package org.example.eksamensprojekt2semester.Model;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class BookingModel {
+    private int booking_id;
+    private int vehicle_id;
+    private String customer_name;
+    private String customer_email;
+    private String customer_phone;
+    private enum lease_type {LIMITED, UNLIMITED};
+    private Date lease_start_date;
+    private Date lease_end_date;
+    private double contract_price;
+    private boolean advance_buyer;
+
+
+    public BookingModel(int booking_id, int vehicle_id, String customer_name, String customer_email, String customer_phone, Date lease_start_date, Date lease_end_date, double contract_price, boolean advance_buyer) {
+        this.booking_id = booking_id;
+        this.vehicle_id = vehicle_id;
+        this.customer_name = customer_name;
+        this.customer_email = customer_email;
+        this.customer_phone = customer_phone;
+        this.lease_start_date = lease_start_date;
+        this.lease_end_date = lease_end_date;
+        this.contract_price = contract_price;
+        this.advance_buyer = advance_buyer;
+    }
+
+
+    public BookingModel(int vehicle_id, String customer_name, String customer_email, String customer_phone, Date lease_start_date, Date lease_end_date, double contract_price, boolean advance_buyer) {
+        this.vehicle_id = vehicle_id;
+        this.customer_name = customer_name;
+        this.customer_email = customer_email;
+        this.customer_phone = customer_phone;
+        this.lease_start_date = lease_start_date;
+        this.lease_end_date = lease_end_date;
+        this.contract_price = contract_price;
+        this.advance_buyer = advance_buyer;
+    }
+
+    public int getBooking_id() {
+        return booking_id;
+    }
+
+    public void setBooking_id(int booking_id) {
+        this.booking_id = booking_id;
+    }
+
+    public int getVehicle_id() {
+        return vehicle_id;
+    }
+
+    public void setVehicle_id(int vehicle_id) {
+        this.vehicle_id = vehicle_id;
+    }
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    public void setCustomer_name(String customer_name) {
+        this.customer_name = customer_name;
+    }
+
+    public String getCustomer_email() {
+        return customer_email;
+    }
+
+    public void setCustomer_email(String customer_email) {
+        this.customer_email = customer_email;
+    }
+
+    public String getCustomer_phone() {
+        return customer_phone;
+    }
+
+    public void setCustomer_phone(String customer_phone) {
+        this.customer_phone = customer_phone;
+    }
+
+    public Date getLease_start_date() {
+        return lease_start_date;
+    }
+
+    public void setLease_start_date(Date lease_start_date) {
+        this.lease_start_date = lease_start_date;
+    }
+
+    public Date getLease_end_date() {
+        return lease_end_date;
+    }
+
+    public void setLease_end_date(Date lease_end_date) {
+        this.lease_end_date = lease_end_date;
+    }
+
+    public double getContract_price() {
+        return contract_price;
+    }
+
+    public void setContract_price(double contract_price) {
+        this.contract_price = contract_price;
+    }
+
+    public boolean isAdvance_buyer() {
+        return advance_buyer;
+    }
+
+    public void setAdvance_buyer(boolean advance_buyer) {
+        this.advance_buyer = advance_buyer;
+    }
+}
--- src/main/java/org/example/eksamensprojekt2semester/Model/CarModel.java
+++ src/main/java/org/example/eksamensprojekt2semester/Model/CarModel.java
@@ -1,4 +1,101 @@
 package org.example.eksamensprojekt2semester.Model;
 
+
 public class CarModel {
+        private int car_model_id;
+        private int model_year;
+        private String brand;
+        private String model;
+        private String car_emission;
+        private CarEquipment car_equipment;
+        private double steel_price;
+        private int registration_fee;
+
+
+    public CarModel(int car_model_id, int model_year, String brand, String model, String car_emission, CarEquipment car_equipment, double steel_price, int registration_fee) {
+        this.car_model_id = car_model_id;
+        this.model_year = model_year;
+        this.brand = brand;
+        this.model = model;
+        this.car_emission = car_emission;
+        this.car_equipment = car_equipment;
+        this.steel_price = steel_price;
+        this.registration_fee = registration_fee;
+    }
+
+    public CarModel(int model_year, String brand, String model, String car_emission, CarEquipment car_equipment, double steel_price, int registration_fee) {
+        this.model_year = model_year;
+        this.brand = brand;
+        this.model = model;
+        this.car_emission = car_emission;
+        this.car_equipment = car_equipment;
+        this.steel_price = steel_price;
+        this.registration_fee = registration_fee;
+    }
+
+    public int getCar_model_id() {
+        return car_model_id;
+    }
+
+    public void setCar_model_id(int car_model_id) {
+        this.car_model_id = car_model_id;
+    }
+
+    public int getModel_year() {
+        return model_year;
+    }
+
+    public void setModel_year(int model_year) {
+        this.model_year = model_year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCar_emission() {
+        return car_emission;
+    }
+
+    public void setCar_emission(String car_emission) {
+        this.car_emission = car_emission;
+    }
+
+    public CarEquipment getCar_equipment() {
+        return car_equipment;
+    }
+
+    public void setCar_equipment(CarEquipment car_equipment) {
+        this.car_equipment = car_equipment;
+    }
+
+    public double getSteel_price() {
+        return steel_price;
+    }
+
+    public void setSteel_price(double steel_price) {
+        this.steel_price = steel_price;
+    }
+
+    public int getRegistration_fee() {
+        return registration_fee;
+    }
+
+    public void setRegistration_fee(int registration_fee) {
+        this.registration_fee = registration_fee;
+    }
 }
+
+
